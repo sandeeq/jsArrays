7a07eca7057a9ea53554d349e9560c18076f9622
sandeeq <sandeeq@gmail.com>
2021-02-22 08:15:47 -0800

    Added array stuff

---
+++ script/script.js
@@ -0,0 +1,14 @@
+// console.log('hello')
+
+let items =['pencil','hat','book'];
+
+console.log(items[0]);
+console.log(items[2]);
+console.log(items.length);
+items.push('laptop');
+console.log(items[3]);
+items.pop();
+console.log(items);
+
+items[4]='suitcase';
+console.log(items);
